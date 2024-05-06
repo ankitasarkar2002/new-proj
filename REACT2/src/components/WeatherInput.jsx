56608e3c000e5cc5a1aad5386238c7810b8bcc31
Ankita Sarkar <ankita.sarkar.cs24@iilmcet.ac.in>
2024-05-07 00:09:32 +0530

    updated

--- REACT2/src/components/WeatherInput.jsx
+++ REACT2/src/components/WeatherInput.jsx
@@ -1,32 +1,30 @@
-import React,{useState,useEffect} from 'react'
-import Button from './Button'
+import React,{useEffect, useState} from 'react'
 
 
 export default function WeatherInput() {
-    const [city,setCity]=useState('');
-    const [weather,setWeather]=useState("mumbai");
+    const[city,setCity]=useState("pune");
+    const [weather,setWeather]=useState('');
+
 
     useEffect(()=>{
-        const fetchApi=async()=>{
-            const url=`https://api.openweathermap.org/data/2.5/weather?q=${weather}&appid=412abe098a6b0e84a2d30abf21da7d0c`
-            const response= await fetch(url)
-            // console.log(response)
-            const resJson= await response.json();
-            // console.log(resJson)
-            setCity(resJson.main)
-           
 
+        const fetchApi= async()=>{
+            const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=412abe098a6b0e84a2d30abf21da7d0c`
+           const response = await fetch(url);
+        //    console.log(response)
+           const resJson= await response.json();
+        //    console.log(resJson)
+           setWeather(resJson.main)
 
         };
         fetchApi();
-        
-
- },[weather] );
+    },[city]);
+   
     
       
     const handleOnChange=(event)=>{
-        // console.log("on change")
-        setWeather(event.target.value);
+        
+    setCity(event.target.value)
     };
 
 
@@ -41,31 +39,27 @@ export default function WeatherInput() {
                 <div className="input-group mb-3 input my-3"  >
                     <input type="text" className="form-control" placeholder="Enter City Here" aria-label="Enter City Here" aria-describedby="button-addon2"
                     onChange={handleOnChange}
-                    value={weather}/>
+                    value={city}
+                    />
                 </div>
-                {/* <div className="button">
-                    <Button text={"Info"} />
-                </div> */}
+                
                 </form>
 
-                {!city?(
-                    <p>no data fount </p>
+                {!weather?(
+                    <p>no data found </p>
                 ):(
 
                     <div className="info my-3">
-                    <h1 className='location'>{weather}</h1>
-                    <h1 className='temp'>{city.temp}</h1>
+                    <h1 className='location'><b>{city}</b></h1>
+                    <h3 className='temp '>Temperature : {weather.temp}</h3>
+                
+                    <h5 className='maxTemp'>Maximum Temp : {weather.temp_max}</h5>
+                    <h5 className='minTemp'>Minimum Temp :  {weather.temp_min}</h5>
+
                     </div>
-                )
+                 )}
                 
             
-            }
-
-
-               
-                
-            
-
             </div>
         </>
     )
